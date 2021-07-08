3f06818caf7c56d373a7e76e546847d0da84671d
fullDev9233 <jimyy9211@gmail.com>
2021-07-08 17:54:08 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+
+const Home = lazy(() => import('./components/Home'));
+const About = lazy(() => import('./components/About'));
+
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/about' component={About}/>
+      </Switch>
+    </Suspense>
+  </Router>
+);
+
+export default App;
---
+++ src/components/Home/index.tsx
@@ -0,0 +1,9 @@
+import styles from './Home.module.scss';
+
+const Home = () => (
+  <div className={styles.root}>
+    Home
+  </div>
+);
+
+export default Home;
